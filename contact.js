4b47c6a3a3251fdc0ef2d98b2988024eabb2a676
karthikrajasrc <karthikrajasrc@gmail.com>
2026-02-02 18:09:05 +0530

    Added Response for contact

--- contact.js
+++ contact.js
@@ -1,45 +1,67 @@
+(function () {
+  emailjs.init("VBcYPl6MCDUv5b5Ug");
+})();
+
 const form = document.querySelector("#contact-form");
 const userinputname = document.querySelector("#userinput-name");
 const userinputphone = document.querySelector("#userinput-phone");
+const userinputquery = document.querySelector("#userinput-query");
 
 const nameError = document.querySelector("#name-error");
 const phoneError = document.querySelector("#phone-error");
 
 
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    let regrexname = /^(?=.*[A-Za-z])[A-Za-z ]+$/;
-    let regrexphone = /^[0-9]{10}$/;
+  let regrexname = /^(?=.*[A-Za-z])[A-Za-z ]+$/;
+  let regrexphone = /^[0-9]{10}$/;
 
-    let valid = true;
+  let valid = true;
 
-    if (!regrexname.test(userinputname.value.trim())) {
-        nameError.textContent = "Enter a valid name";
-        valid = false;
-    } else {
-        nameError.textContent = "";
-    }
+  if (!regrexname.test(userinputname.value.trim())) {
+    nameError.textContent = "Enter a valid name";
+    valid = false;
+  } else {
+    nameError.textContent = "";
+  }
     
-    if (!regrexphone.test(userinputphone.value.trim())) {
-        phoneError.textContent = "Enter a valid 10-digit phone number";
-        valid = false;
-    } else {
-        phoneError.textContent = "";
-    }
+  if (!regrexphone.test(userinputphone.value.trim())) {
+    phoneError.textContent = "Enter a valid 10-digit phone number";
+    valid = false;
+  } else {
+    phoneError.textContent = "";
+  }
+
+    if (!valid) return;
     
-    if (valid) {
     const submitBtn = form.querySelector("button[type='submit']");
     submitBtn.disabled = true;
     submitBtn.textContent = "Submitting...";
-
-    setTimeout(() => {
-      form.innerHTML = `
-        <p id="success-message" style="font-weight: bold;font-size: 18px;">
-          ✅ Form submitted successfully
-        </p>
-      `;
-    }, 2000);
-  }
-});
-
+      
+  const templateParams = {
+    title: "Contact Form Query",
+     name: userinputname.value.trim(),
+      from_name: userinputname.value.trim(),
+      phone: userinputphone.value.trim(),
+      message: userinputquery.value.trim()
+    };
+    emailjs.send(
+      "service_xpdlcvs", 
+      "template_6c36qoe",
+      templateParams
+    )
+      .then(() => {
+        form.innerHTML = `
+      <p id="success-message" style="font-weight: bold; font-size: 18px;">
+        ✅ Form submitted successfully
+      </p>
+    `;
+      })
+      .catch((error) => {
+        console.error("EmailJS Error:", error);
+        submitBtn.disabled = false;
+        submitBtn.textContent = "Submit";
+        alert("❌ Failed to send message. Please try again.");
+      });
+  });
